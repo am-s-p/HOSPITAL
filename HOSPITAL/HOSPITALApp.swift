d2fc3aee68048e621c472501cdb5534886a676fe
Ammey Paathak <136736363+am-s-p@users.noreply.github.com>
2024-03-18 22:04:24 +0530

    Initial Commit

---
+++ HOSPITAL/HOSPITALApp.swift
@@ -0,0 +1,17 @@
+//
+//  HOSPITALApp.swift
+//  HOSPITAL
+//
+//  Created by Ammey Paathak on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HOSPITALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
